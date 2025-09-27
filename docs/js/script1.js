8c36759310a0c3456784c145bcc9c0ddd95d78b3
ShahovaEV <lenka.shakhova666@gmail.com>
2025-09-27 12:25:59 +0300

    js2

--- docs/js/script1.js
+++ docs/js/script1.js
@@ -1,4 +1,7 @@
-        var btn1 = document.getElementById("btn-1")
-        btn1.addEventListener("click" , () => (
-            alert("Hello world by ShahovaEV")
-        ))
+        var btn1 = document.getElementById("btn-1");
+        var input_name = document.getElementById("input-name");
+        btn1.addEventListener("click" , () => {
+            var name = input_name.value;
+            console.log(name);
+            alert(`Hello ${name}!`); 
+        });
